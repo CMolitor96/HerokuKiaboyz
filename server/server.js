5f3bb1dd25bfbb818fe813a57795c13d50fa0a41
Charles Molitor <charlie.molitor.38@gmail.com>
2023-08-29 09:18:15 -0400

    All changes for pushing to heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -12,10 +14,10 @@ app.use(express.json());
 // }
 
 
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 
 // app.use(express.static(path.resolve(__dirname, 'build')));
